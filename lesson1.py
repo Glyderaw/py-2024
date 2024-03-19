3ff953e08671ea34e3fe09673ed6c8ce8fcc8bf7
Glyderaw <118878509+Glyderaw@users.noreply.github.com>
2024-03-19 18:27:41 +0530

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,18 @@
+print("Hello whats your name")
+name=input()
+print("Hii",name,"Its nice to meet you")
+print("enter your age")
+age=int (input())
+print(" you are ",name," your age is ",age)
+print("where do you live?")
+place=input()
+print("You live in ",place,"Wow that a nice place")
+print("Tell me some nice places  you like")
+place1=input()
+place2=input()
+place3=input()
+place4=input()
+places=[place1,place2,place3,place4]
+print("i see you so these are the places you like",places)
+for place in places:
+    print(places)
